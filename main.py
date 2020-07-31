d4897cfb068c574ac08118ef23cee4d2eb89ddf5
evie ellis <lski@xhda.tk>
2020-07-31 21:09:19 +0100

    Made max delay 30

--- main.py
+++ main.py
@@ -12,22 +12,27 @@ DELAY_BASE = 5
 
 client = discord.Client()
 
+
 def calculateDelayTime(text):
     totalDelay = DELAY_BASE
     for x in text:
         if x == " ":
             totalDelay += 1
+    if totalDelay > 30:
+        totalDelay = 30
     print(totalDelay)
     return totalDelay
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.channel.name== "catras-diary":
+    if message.channel.name == "catras-diary":
         archiveChannel = client.get_channel(738415449582075924)
-        await archiveChannel.send(message.author.name +": "+ message.content)
+        await archiveChannel.send(message.author.name + ": " + message.content)
         time.sleep(calculateDelayTime(message.content))
         await message.delete()
+
 
 client.run(TOKEN)
