47d9518d157cf96ca705a271e5f4c5151d97d5d0
evie ellis <lski@xhda.tk>
2020-08-03 16:11:07 +0100

    lighthope chat kinda works now

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ import time
 import discord
 from dotenv import load_dotenv
 
+from wit import Wit
+
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+DSC_TOKEN = os.getenv('DISCORD_TOKEN')
+WIT_TOKEN = os.getenv('WIT_TOKEN')
+
 
 RCHANNEL_ID = "catras-diary"
 DELAY_BASE = 5
 
-client = discord.Client()
+dscClient = discord.Client()
 
 
 def calculateDelayTime(text):
@@ -26,21 +30,56 @@ def calculateDelayTime(text):
 
 
 async def log(string):
-    logChannel = client.get_channel(739170928805806202)
+    logChannel = dscClient.get_channel(739170928805806202)
     await logChannel.send(string)
 
-@client.event
+@dscClient.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == dscClient.user:
         return
     if message.channel.name == "catras-diary":
-        archiveChannel = client.get_channel(738415449582075924)
+        archiveChannel = dscClient.get_channel(738415449582075924)
         await archiveChannel.send(message.author.name + ": " + message.content)
         time.sleep(calculateDelayTime(message.content))
         await message.delete()
 
-@client.event
+    if "lighthope" in message.content:
+        response = witClient.message(msg=message.content)
+        await handle_message(response, message.channel)
+
+@dscClient.event
 async def on_ready():
     await log("Lighthope OS is starting...")
 
-client.run(TOKEN)
+def first_trait_value(traits, trait):
+    """
+    Returns first trait value
+    """
+    if trait not in traits:
+        return None
+    val = traits[trait][0]['value']
+    if not val:
+        return None
+    return val
+
+
+async def handle_message(response, channel):
+    """
+    Customizes our response to the message and sends it
+    """
+    # Checks if user's message is a greeting
+    # Otherwise we will just repeat what they sent us
+    greetings = first_trait_value(response['traits'], 'wit$greetings')
+    if greetings:
+        text = "hello!"
+    else:
+        text = "Query not recognised"
+    # send message
+    await channel.send(text)
+
+
+
+# Setup Wit Client
+witClient = Wit(access_token=WIT_TOKEN)
+
+dscClient.run(DSC_TOKEN)
